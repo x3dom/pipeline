2b2b76d1ec3ce5c944c55bcf35f9cf019408c6ea
aha <andreas.aderhold@igd.fraunhofer.de>
2012-07-26 11:49:36 +0200

    Back to browser auth

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,3 @@ import os, sys
 # fix this with packaged deploy
 sys.path.insert(0, '/var/www/modelconvert/apps/modelconvert')
 from application import app as application
-
-from barrel import cooper
-logins = [('x3dom', 'x3dom')]
-
-
-application = cooper.basicauth(users=logins)(application)
