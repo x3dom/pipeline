bbec53f2ccc781fbcdea1a421304d8e3b9eb577b
aha <andreas.aderhold@igd.fraunhofer.de>
2012-07-26 11:13:57 +0200

    Added barrel for simpl auth

--- wsgi.py
+++ wsgi.py
@@ -2,3 +2,9 @@ import os, sys
 # fix this with packaged deploy
 sys.path.insert(0, '/var/www/modelconvert/apps/modelconvert')
 from application import app as application
+
+from barrel import cooper
+logins = [('x3dom', 'x3dom')]
+
+
+application = cooper.formauth(users=logins)(application)
