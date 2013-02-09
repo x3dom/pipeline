ab1f2971e05ad1ed8e98d2bb09c9c93f12dea1d8
aha <andreas.aderhold@igd.fraunhofer.de>
2013-02-09 12:14:22 +0100

    More generic wsgi file; Added example of apache config mod_wsgi

--- modelconvert/wsgi.py
+++ modelconvert/wsgi.py
@@ -16,6 +16,13 @@ import sys
 
 # This is currently deployment specific and not required
 # with a packaged deploy (since the package is in the Python path)
-sys.path.insert(0, '/var/www/modelconvert/modelconvert')
+BASE_DIR = os.path.join(os.path.dirname(__file__), '..')
+
+if BASE_DIR not in sys.path:
+    sys.path.append(BASE_DIR)
 
 from modelconvert.application import app as application
+
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
